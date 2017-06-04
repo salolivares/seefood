18d202f08462b8833d16e2c0cd589ff09c5c356b
Sal Olivares <git@salolivares.com>
2017-06-04 16:22:32 -0700

    done with iteration 1

---
+++ app/src/main/java/edu/ucsb/cs/cs190i/aviato/seefood/RecipeWebViewActivity.java
@@ -0,0 +1,29 @@
+package edu.ucsb.cs.cs190i.aviato.seefood;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class RecipeWebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("recipe url");
+
+        openWebPage(url);
+    }
+
+    private void openWebPage(String url) {
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl(url);
+    }
+}
