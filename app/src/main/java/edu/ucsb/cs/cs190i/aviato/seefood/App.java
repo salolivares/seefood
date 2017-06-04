a2d850af1675797e4dc41978038d0b69090e9818
Sal Olivares <git@salolivares.com>
2017-06-03 17:01:11 -0700

    image uploading now works.

---
+++ app/src/main/java/edu/ucsb/cs/cs190i/aviato/seefood/App.java
@@ -0,0 +1,49 @@
+package edu.ucsb.cs.cs190i.aviato.seefood;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import okhttp3.OkHttpClient;
+
+/*
+ * Source: https://github.com/Clarifai/clarifai-android-starter/blob/b28ce678ce1a099e7c836a81cbc70760339478a5/app/src/main/java/com/clarifai/android/starter/api/v2/App.java
+ */
+
+public class App extends Application {
+    private static App INSTANCE;
+
+    @NonNull
+    public static App get() {
+        final App instance = INSTANCE;
+        if (instance == null) {
+            throw new IllegalStateException("App has not been created yet!");
+        }
+        return instance;
+    }
+
+    @Nullable
+    private ClarifaiClient client;
+
+    @Override
+    public void onCreate() {
+        System.out.println("HERE");
+        INSTANCE = this;
+        client = new ClarifaiBuilder("gGdtlfx3ALK7wKl-nRTV_9qNQgSzS8AgX-xwk6Nf","-lTxAVv1JnNg7PGE6ZJw8r3q42Us68fzH0L6I_O8").buildSync();
+        super.onCreate();
+    }
+
+    @NonNull
+    public ClarifaiClient clarifaiClient() {
+        final ClarifaiClient client = this.client;
+        if (client == null) {
+            throw new IllegalStateException("Cannot use Clarifai client before initialized");
+        }
+        return client;
+    }
+}
--- app/src/main/java/edu/ucsb/cs/cs190i/aviato/seefood/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs190i/aviato/seefood/MainActivity.java
@@ -1,15 +1,37 @@
 package edu.ucsb.cs.cs190i.aviato.seefood;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.List;
+
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.ConceptModel;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
 
 public class MainActivity extends AppCompatActivity {
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    private static final String APP_TAG = "APP_ME";
+    private String photoFileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +44,114 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                openCamera();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri takenPhotoUri = getPhotoFileUri(photoFileName);
+                final byte[] imageBytes = ClarifaiUtil.retrieveSelectedImage(this, takenPhotoUri);
+                if (imageBytes != null) {
+                    onImagePicked(imageBytes);
+                }
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void onImagePicked(final byte[] imageBytes) {
+        // Now we will upload our image to the Clarifai API
+        setBusy(true);
+
+        new AsyncTask<Void, Void, ClarifaiResponse<List<ClarifaiOutput<Concept>>>>() {
+            @Override protected ClarifaiResponse<List<ClarifaiOutput<Concept>>> doInBackground(Void... params) {
+                // The default Clarifai model that identifies concepts in images
+                final ConceptModel generalModel = App.get().clarifaiClient().getDefaultModels().generalModel();
+
+                // Use this model to predict, with the image that the user just selected as the input
+                return generalModel.predict()
+                        .withInputs(ClarifaiInput.forImage(ClarifaiImage.of(imageBytes)))
+                        .executeSync();
+            }
+
+            @Override protected void onPostExecute(ClarifaiResponse<List<ClarifaiOutput<Concept>>> response) {
+                setBusy(false);
+                if (!response.isSuccessful()) {
+                    showErrorSnackbar("error_while_contacting_api");
+                    return;
+                }
+                final List<ClarifaiOutput<Concept>> predictions = response.get();
+                if (predictions.isEmpty()) {
+                    showErrorSnackbar("no_results_from_api");
+                    return;
+                }
+                //adapter.setData(predictions.get(0).data());
+                System.out.println(predictions.get(0).data());
+                //imageView.setImageBitmap(BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length));
+            }
+
+            private void showErrorSnackbar(String s) {
+                Log.d("App",s);
+            }
+
+        }.execute();
+    }
+
+    private void setBusy(boolean b) {
+
+    }
+
+    private void openCamera() {
+        // New photo
+        photoFileName = "photo" + System.currentTimeMillis() + ".jpg";
+
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName)); // set the image file name
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the Uri for a photo stored on disk given the fileName
+    public Uri getPhotoFileUri(String fileName) {
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+            // Get safe storage directory for photos
+            // Use `getExternalFilesDir` on Context to access package-specific directories.
+            // This way, we don't need to request external read/write runtime permissions.
+            File mediaStorageDir = new File(
+                    getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+            // wrap File object into a content provider
+            // required for API >= 24
+            // See https://guides.codepath.com/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+            return FileProvider.getUriForFile(MainActivity.this, "com.aviato.fileprovider", file);
+        }
+        return null;
+    }
+
+    // Returns true if external storage for photos is available
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        return state.equals(Environment.MEDIA_MOUNTED);
     }
 
     @Override
