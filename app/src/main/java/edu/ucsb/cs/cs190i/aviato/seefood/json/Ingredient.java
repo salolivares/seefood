cb95eed7a074218598b9e15da8f97ef01e5c1d87
Sal Olivares <git@salolivares.com>
2017-06-04 13:42:01 -0700

    recipe api now works

---
+++ app/src/main/java/edu/ucsb/cs/cs190i/aviato/seefood/json/Ingredient.java
@@ -0,0 +1,32 @@
+
+package edu.ucsb.cs.cs190i.aviato.seefood.json;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Ingredient {
+
+    @SerializedName("text")
+    @Expose
+    private String text;
+    @SerializedName("weight")
+    @Expose
+    private Double weight;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+}
